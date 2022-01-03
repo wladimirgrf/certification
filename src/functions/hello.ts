d5e5617c27a69851b246140ddad3c58cad708cea
wladimirgrf <wladimir.code@gmail.com>
2022-01-03 11:00:37 -0300

    feat: serverless deploy script, adjustments

--- src/functions/hello.ts
+++ src/functions/hello.ts
@@ -1,5 +1,5 @@
 
-export const handler = async (event, context, callback) => {
+export const handle = async (event, context, callback) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
