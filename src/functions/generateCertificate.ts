8b9aa4ac98dafdc01b141771b04f20b90f742eb6
wladimirgrf <wladimir.code@gmail.com>
2022-01-07 20:32:07 -0300

    fix: Remove pdf generate, send html to s3

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,8 +1,8 @@
 import path from "path";
 import fs from "fs";
-import chromium from "chrome-aws-lambda";
 import handlebars from "handlebars";
 import dayjs from "dayjs";
+import { S3 } from "aws-sdk";
 
 import { document } from "../utils/dynamodbClient";
 
@@ -43,10 +43,22 @@ const compile = async (data: ITemplate) => {
 export const handle = async (event, context, callback) => {
   const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate;
 
-  const Item = { id, name, grade };
   const TableName = "users_certificates";
 
-  await document.put({ TableName, Item }).promise();
+  const responseQuery = await document.query({
+    TableName,
+    KeyConditionExpression: "id = :id",
+    ExpressionAttributeValues: {
+      ":id": id
+    }
+  }).promise();
+
+  const userAlreadyExists = responseQuery.Items[0];
+
+  if(!userAlreadyExists){
+    const Item = { id, name, grade };
+    await document.put({ TableName, Item }).promise();
+  }
 
   const today = dayjs().format("DD/MM/YYYY");
 
@@ -59,31 +71,25 @@ export const handle = async (event, context, callback) => {
 
   const content = await compile(data);
 
-  const browser = await chromium.puppeteer.launch({
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath
+  const s3 = new S3({
+    credentials: {
+      accessKeyId: process.env.ACCESS_KEY_ID,
+      secretAccessKey: process.env.SECRET_ACCESS_KEY
+    }
   });
 
-  const page = await browser.newPage();
-  await page.setContent(content);
-
-  const path = process.env.IS_OFFLINE ? "certificate.pdf" : null;
-
-  const pdf = await page.pdf({
-    format: "a4",
-    path,
-    landscape: true,
-    printBackground: true,
-    preferCSSPageSize: true,
-  });
-
-  await browser.close();
+  await s3.putObject({
+    Bucket: "serverlesscertification",
+    Key: `${id}.html`,
+    ACL: "public-read",
+    Body: content,
+    ContentType: "text/html; charset=utf-8"
+  }).promise();
 
   return {
     statusCode: 201,
     body: JSON.stringify({
-      message: "Certificate created!"
+      message: "Certificate created!",
     }),
     headers: {
       "Content-Type": "application/json"
