13423314e96ed9c7f4d20946422e1d5f88871aa9
wladimirgrf <wladimir.code@gmail.com>
2022-01-07 16:07:40 -0300

    feat: generateCertificate, handlebars and chrome-aws-lambda

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,9 +1,43 @@
-import { document } from '../utils/dynamodbClient';
+import path from "path";
+import fs from "fs";
+import chromium from "chrome-aws-lambda";
+import handlebars from "handlebars";
+import dayjs from "dayjs";
+
+import { document } from "../utils/dynamodbClient";
 
 interface ICreateCertificate {
   id: string;
   name: string;
   grade: string;
+}
+
+interface ITemplate {
+  id: string;
+  name: string;
+  grade: string;
+  date: string;
+}
+
+const compile = async (data: ITemplate) => {
+  const templatePath = path.join(
+    process.cwd(),
+    "src",
+    "templates",
+    "certificate.hbs"
+  );
+
+  const medalPath = path.join(
+    process.cwd(),
+    "src",
+    "templates",
+    "selo.png"
+  );
+
+  const html = fs.readFileSync(templatePath, "utf-8");
+  const medal = fs.readFileSync(medalPath, "base64");
+
+  return handlebars.compile(html)({ ...data, medal });
 }
 
 export const handle = async (event, context, callback) => {
@@ -12,9 +46,39 @@ export const handle = async (event, context, callback) => {
   const Item = { id, name, grade };
   const TableName = "users_certificates";
 
-  const result = await document.put({ TableName, Item }).promise();
+  await document.put({ TableName, Item }).promise();
 
-  console.log(result);
+  const today = dayjs().format("DD/MM/YYYY");
+
+  const data: ITemplate = {
+    id,
+    name,
+    grade,
+    date: today,
+  }
+
+  const content = await compile(data);
+
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath
+  });
+
+  const page = await browser.newPage();
+  await page.setContent(content);
+
+  const path = process.env.IS_OFFLINE ? "certificate.pdf" : null;
+
+  const pdf = await page.pdf({
+    format: "a4",
+    path,
+    landscape: true,
+    printBackground: true,
+    preferCSSPageSize: true,
+  });
+
+  await browser.close();
 
   return {
     statusCode: 201,
